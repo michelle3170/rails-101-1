e05c7b834ff4672c582d34107183e144d2e094e3
michell3170 <michelle3170@qq.com>
2016-12-26 05:18:45 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,19 +27,45 @@ class GroupsController < ApplicationController
     else
       render :new
      end
-  end
+   end
 
-  def update
+   def update
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
       render :edit
-    end
+     end
+   end
+
+   def destroy
+     @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+   end
+
+  def join
+    @group = Group.find(params[:id])
+
+   if !current_user.is_member_of?(@group)
+     current_user.join!(@group)
+     flash[:notice] = "加入本讨论版成功！"
+   else
+     flash[:warning] = "你已经是本讨论版成员了！"
+   end
+
+     redirect_to group_path(@group)
   end
 
-  def destroy
-    @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
+  def quit
+      @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+    has_many :groups
+    has_many :posts
     has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
 
   def join!(group)
     participated_groups << group
@@ -15,9 +19,5 @@ class User < ApplicationRecord
 
   def quit!(group)
     participated_groups.delete(group)
-  end
-  
-  def is_member_of?(group)
-    participated_groups.include?(group)
   end
 end
